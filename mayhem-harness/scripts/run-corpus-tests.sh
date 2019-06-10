2b852287e657674920b21180d3083ff3d8c379e8
Sheldon Warkentin <sheldonw@forallsecure.com>
2019-06-10 15:00:47 -0600

    Improve output for running mayhem corpus tests

--- mayhem-harness/scripts/run-corpus-tests.sh
+++ mayhem-harness/scripts/run-corpus-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -euox pipefail
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # Work in the mayhem-harness folder
@@ -10,6 +8,7 @@ cd $DIR/..
 # Run each corpus test to gather coverage
 for test in corpus/*
 do
+  echo "Running mayhem test: $test"
   # Do not run web server for more than 5sec per test
   build/mayhem-harness-exe 5000 & sleep 1; nc 127.0.0.1 8000 < $test
 done
